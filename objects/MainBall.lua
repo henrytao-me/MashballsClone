a1411b605ccfe9bd1c65ab293221f8354d0b7dcd
Henry Tao <hi@henrytao.me>
2015-02-26 20:48:47 +0700

    [MASTER] Added physical ball

--- objects/MainBall.lua
+++ objects/MainBall.lua
@@ -10,6 +10,56 @@ function MainBall:init(level, x, y)
 	self.bitmap:setAnchorPoint(0.5,0.5)
 	self:addChild(self.bitmap)
 	
+	self:addEventListener(Event.MOUSE_DOWN, self.onMouseDown, self)
+	self:addEventListener(Event.MOUSE_MOVE, self.onMouseMove, self)
+	self:addEventListener(Event.MOUSE_UP, self.onMouseUp, self)
+end
+
+function MainBall:onMouseDown(e) 
+	e:stopPropagation()
+	self.startX = self:getX()
+	self.startY = self:getY()
+	self.isDragged = true
+end
+
+function MainBall:onMouseMove(e) 
+	if self.isDragged then
+		e:stopPropagation()
+		local r = 120
+		local xVect = (e.x-self.startX)
+		local yVect = (e.y-self.startY)
+		local length = math.sqrt(xVect*xVect + yVect*yVect)
+		if length <= r then
+			self:setPosition(e.x, e.y)
+		else
+			local coef = math.sqrt((r*r)/(xVect*xVect+yVect*yVect))
+			self:setX(self.startX+xVect*coef)
+			self:setY(self.startY+yVect*coef)
+		end
+	end
+end
+
+function MainBall:onMouseUp(e)
+     if self.isDragged  then
+       e:stopPropagation()
+       self.isDragged = false
+       self:createBody()
+       --define strength of slingshot
+       local strength = 10
+       --calculate force vector based on strength
+       --and distance of pull
+       local xVect = (self.startX - self:getX())*strength
+       local yVect = (self.startY - self:getY())*strength
+       --applye impulse to target
+       self.body:applyLinearImpulse(xVect, yVect, self:getX(),
+         self:getY())
+       self:removeEventListener(Event.MOUSE_DOWN, self.onMouseDown, self)
+       self:removeEventListener(Event.MOUSE_MOVE, self.onMouseMove, self)
+       self:removeEventListener(Event.MOUSE_UP, self.onMouseUp, self)
+     end
+end
+
+function MainBall:createBody ()
 	local radius = self.bitmap:getWidth() / 2 * 0.85
 	
 	--create box2d physical object
