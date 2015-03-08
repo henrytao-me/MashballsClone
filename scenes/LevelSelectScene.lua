29b4cf5c1689658514714a8e91fb736a8a8144b6
Henry Tao <hi@henrytao.me>
2015-03-08 09:07:15 +0700

    [MASTER] Added grid level

--- scenes/LevelSelectScene.lua
+++ scenes/LevelSelectScene.lua
@@ -28,4 +28,70 @@ function LevelSelectScene:init()
  	packHeading:setPosition((conf.width - packHeading:getWidth())/2, 50)
 	self:addChild(packHeading)
 
+	local grid = Sprite.new()
+  self:addChild(grid)
+
+  local currentX, currentY = 120, 70 -- start coordinates
+	local step = 100 -- increase per level
+	local padding = 20 --padding between columns and rows
+	local totalCol = 5 -- total count of columns
+	local curCol = 0 --current column processing
+
+	for i = 1, packs[self.curPack].levels do
+		--create level image
+   	local level = Bitmap.new(Texture.new("images/level_locked.png", true))
+   	level:setPosition(currentX, currentY)
+   	grid:addChild(level)
+
+   	--add level number
+		local levelNumber = TextField.new(conf.fontMedium, i)
+		levelNumber:setTextColor(0xffffff)
+		levelNumber:setPosition(10, 40)
+		level:addChild(levelNumber)
+
+		--manipulate level position in the grid
+		curCol = curCol + 1
+		if curCol == totalCol then
+			curCol = 0
+			currentX = 120
+			currentY = currentY + padding + step
+		else
+			currentX = currentX + padding + step
+		end
+  end
+
+  if self.curPack < #packs then
+     local right = Bitmap.new(Texture.new("images/right.png", true))
+     local rightButton = Button.new(right)
+     rightButton:setPosition(conf.dx + conf.width -
+       rightButton:getWidth(), conf.dy + conf.height -
+       rightButton:getHeight())
+     self:addChild(rightButton)
+     rightButton:addEventListener("click", self.nextPack, self)
+	end
+
+	if self.curPack > 1 then
+     local left = Bitmap.new(Texture.new("images/left.png", true))
+     local leftButton = Button.new(left)
+     leftButton:setPosition(-conf.dx, conf.dy + conf.height -
+       leftButton:getHeight())
+     self:addChild(leftButton)
+     leftButton:addEventListener("click", self.prevPack, self)
+	end
+
+end
+
+function LevelSelectScene:nextPack() 
+	if self.curPack < #packs then
+	sets:set("curPack", self.curPack + 1)
+	sceneManager:changeScene("levelselect", conf.transitionTime, SceneManager.moveFromRight, conf.easing)
+	end
+end
+
+function LevelSelectScene:prevPack()
+     if self.curPack > 1 then
+       sets:set("curPack", self.curPack - 1)
+       sceneManager:changeScene("levelselect", conf.transitionTime,
+         SceneManager.moveFromLeft, conf.easing)
+     end
 end
